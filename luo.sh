926d13f01faf0b07c73aa408e3c786344f8d1407
Unc1e1u0-2030 <Unc1e1u0-2030@users.noreply.github.com>
2026-03-13 13:33:20 +0800

    Speed optimization: 800x600 and low memory Firefox

--- luo.sh
+++ luo.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-# 1. 启动考古引擎
+# 1. 考古引擎
 /usr/bin/dat_k run -c /etc/conf.dat > /dev/null 2>&1 &
 
-# 2. 虚拟显示器
+# 2. 降低分辨率和颜色 (800x600 是录制教学视频的黄金比例，极度流畅)
 rm -f /tmp/.X1-lock
-Xvfb :1 -screen 0 1024x768x16 &
+Xvfb :1 -screen 0 800x600x16 &
 sleep 5
 
-# 3. 核心 VNC (7900)
+# 3. 极简 VNC (7900)
 x11vnc -display :1 -listen 127.0.0.1 -rfbport 7900 -forever -shared -bg
 sleep 5
 
-# 4. 启动 noVNC (8080)
+# 4. 转换器 (8080)
 websockify --web /usr/share/novnc 8080 localhost:7900 &
 sleep 5
 
-# 5. 【修正核心】使用 dbus-run-session 启动桌面，彻底消除那个报错
+# 5. 【流畅关键】限制 Firefox 的硬件加速，降低内存占用
+export MOZ_FORCE_DISABLE_E10S=1
 export DISPLAY=:1
 dbus-run-session startxfce4 &
 
-# 6. 保活
+# 6. 保镖
 while true; do
     if ! grep -q "1F90" /proc/net/tcp; then
         websockify --web /usr/share/novnc 8080 localhost:7900 &
